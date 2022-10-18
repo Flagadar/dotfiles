fe444c05769cae7f557e98e50ef3cb661ac278ef
Flagadar <74012222+Flagadar@users.noreply.github.com>
2022-10-18 17:36:01 +0200

    Update install.sh

--- PostInstall/install.sh
+++ PostInstall/install.sh
@@ -11,9 +11,8 @@ sudo apt install xorg lightdm -y
 # Install dependencies
 sudo apt install git libxext-dev libxcb1-dev libxcb-damage0-dev libxcb-xfixes0-dev libxcb-shape0-dev libxcb-render-util0-dev libxcb-render0-dev libxcb-randr0-dev libxcb-composite0-dev libxcb-image0-dev libxcb-present-dev libxcb-xinerama0-dev libxcb-glx0-dev libpixman-1-dev libdbus-1-dev libconfig-dev libgl-dev libegl-dev libpcre2-dev libpcre3-dev libevdev-dev uthash-dev libev-dev libx11-xcb-dev meson -y
 # Build / Install
-mkdir picom
-cd picom
 sudo git clone https://github.com/yshui/picom
+cd picom
 sudo git submodule update --init --recursive
 sudo meson --buildtype=release . build
 sudo ninja -C build install
