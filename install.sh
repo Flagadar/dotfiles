dc801c52a7c5f53c979c6bcbffb616ebcb2c85f1
Flagadar <74012222+Flagadar@users.noreply.github.com>
2022-10-18 20:33:08 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -30,10 +30,26 @@ sudo python3 setup.py install
 cd ..
 
 # - Alacritty
-sudo apt install cmake curl libfontconfig1-dev -y
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-#exec bash
-cargo install alacritty
+sudo apt install cmake curl libfontconfig1-dev cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev python3 -y
+git clone https://github.com/alacritty/alacritty
+cd alacritty
+cargo build --release --no-default-features --features=x11
+infocmp alacritty
+sudo tic -xe alacritty,alacritty-direct extra/alacritty.info
+sudo cp target/release/alacritty /usr/local/bin # or anywhere else in $PATH
+sudo cp extra/logo/alacritty-term.svg /usr/share/pixmaps/Alacritty.svg
+sudo desktop-file-install extra/linux/Alacritty.desktop
+sudo update-desktop-database
+sudo mkdir -p /usr/local/share/man/man1
+gzip -c extra/alacritty.man | sudo tee /usr/local/share/man/man1/alacritty.1.gz > /dev/null
+gzip -c extra/alacritty-msg.man | sudo tee /usr/local/share/man/man1/alacritty-msg.1.gz > /dev/null
+mkdir -p ${ZDOTDIR:-~}/.zsh_functions
+echo 'fpath+=${ZDOTDIR:-~}/.zsh_functions' >> ${ZDOTDIR:-~}/.zshrc
+cp extra/completions/_alacritty ${ZDOTDIR:-~}/.zsh_functions/_alacritty
+echo "source $(pwd)/extra/completions/alacritty.bash" >> ~/.bashrc
+mkdir -p ~/.bash_completion
+cp extra/completions/alacritty.bash ~/.bash_completion/alacritty
+echo "source ~/.bash_completion/alacritty" >> ~/.bashrc
 
 # - Helix
 git clone https://github.com/helix-editor/helix
