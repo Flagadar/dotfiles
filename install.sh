5bada77d97f4747a8b4422b0ee41501f476ac0cc
Flagadar <74012222+Flagadar@users.noreply.github.com>
2022-10-18 19:00:19 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -29,6 +29,12 @@ cd qtile-extras
 sudo python3 setup.py install
 cd ..
 
+# - Alacritty
+sudo apt install curl -y
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+exec bash
+cargo install alacritty
+
 # - Copy config files
 sudo mkdir ~/.config
 sudo cp config/* ~/.config/
