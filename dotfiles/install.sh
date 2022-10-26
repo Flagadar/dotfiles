14efa191be6ba3f127164e6b0a6a6cab769f5d48
Flagadar <flagadar@pop-os.localdomain>
2022-10-26 16:12:04 +0200

    Fix: Alacritty install

--- dotfiles/install.sh
+++ dotfiles/install.sh
@@ -43,8 +43,8 @@ git clone https://github.com/alacritty/alacritty
   sudo tic -xe alacritty,alacritty-direct extra/alacritty.info
   sudo cp target/release/alacritty /usr/local/bin # or anywhere else in $PATH
   sudo cp extra/logo/alacritty-term.svg /usr/share/pixmaps/Alacritty.svg
-  sudo desktop-file-install extra/linux/Alacritty.desktop
-  sudo update-desktop-database
+  sudo sh desktop-file-install extra/linux/Alacritty.desktop
+  sudo sh update-desktop-database
   sudo mkdir -p /usr/local/share/man/man1
   gzip -c extra/alacritty.man | sudo tee /usr/local/share/man/man1/alacritty.1.gz > /dev/null
   gzip -c extra/alacritty-msg.man | sudo tee /usr/local/share/man/man1/alacritty-msg.1.gz > /dev/null
